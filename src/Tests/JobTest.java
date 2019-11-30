17515799a3caf6e84be5b09446ea890f6a2141da
Elizabeth Ernst <eliernst@gmail.com>
2019-11-29 18:03:36 -0600

    4.2

---
+++ src/Tests/JobTest.java
@@ -0,0 +1,11 @@
+package Tests;
+import org.junit.Before;
+
+public class JobTest {
+    JobTest test_Job;
+    @Before
+    public void createJobObject(){
+        JobTest test_Job = new JobTest();
+    }
+
+}
