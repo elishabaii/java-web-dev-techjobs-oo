497ec86c53bcd52927e468e5282a9ba758987a7f
Elizabeth Ernst <eliernst@gmail.com>
2019-12-03 17:55:59 -0600

    toString test

---
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -0,0 +1,60 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        this.id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PositionType)) return false;
+        PositionType that = (PositionType) o;
+        return getId() == that.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public static void setNextId(int nextId) {
+        JobField.nextId = nextId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
+}
